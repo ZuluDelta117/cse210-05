212e2f1a480d57f92a140614c1dba77ab119c729
SethHartman13 <familyhartseth@aol.com>
2022-11-18 18:14:22 -0700

    stopwatch actually done

--- game/services/stopwatch_services.py
+++ game/services/stopwatch_services.py
@@ -2,11 +2,11 @@ import time
 
 class Stopwatch_services():
     def __init__(self):
-        self.start = 0
+        self.current = 0
 
     @property
     def add_second(self):
-        seconds = check_time.start
+        seconds = check_time.current
         time.sleep(1)
         seconds =+ 1
         return seconds
@@ -16,7 +16,4 @@ check_time = Stopwatch_services()
 def return_value():
     runner = True
     while runner == True:
-        print(check_time.start)
-        check_time.start += check_time.add_second
-
-print(return_value())
+        check_time.current += check_time.add_second
